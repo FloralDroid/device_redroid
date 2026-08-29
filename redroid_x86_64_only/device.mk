75e6a2ad81bceff9e08009d3b442f91a044565a7
5656565566 <contact@home56.top>
2026-08-29 11:24:32 +0800

    redroid: install NativeBridge guest packages

--- redroid_x86_64_only/device.mk
+++ redroid_x86_64_only/device.mk
@@ -7,4 +7,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 $(call inherit-product, system/floral/nativebridge/nativebridge.mk)
 
+# Build and install AOSP's guest userspace. The bionic patch keeps the host
+# NativeBridge libc variant non-installable so guest libc owns the libc.so path.
+PRODUCT_SOONG_NAMESPACES += frameworks/libs/native_bridge_support/libc
+include frameworks/libs/native_bridge_support/native_bridge_support.mk
+PRODUCT_PACKAGES += $(NATIVE_BRIDGE_PRODUCT_PACKAGES)
+
 $(call inherit-product, device/redroid-prebuilts/prebuilts_x86.mk)
